f99d28a741f600c4d52e40d92193e3f682a4f5d2
Russell Rosario <rosariorussell@gmail.com>
2018-02-28 23:21:18 -0500

    added questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,58 @@
 var timer = 90;
 // timer 0, end game
 
-
+var questionArray = [
+	{
+		question: "What team won the very first NBA game?",
+		choices: ["New York Knicks", "Philadelphia Warriors", "Toronto Huskies", "Chicago Stags"],
+		correctChoice: "New York Knicks",
+	},
+	{
+		question: "What NBA player scored 100 points on March 2, 1962?",
+		choices: ['Kareem Abdul-Jabbar', 'Wilt Chamberlain', 'Elgin Baylor', 'Bill Russell', ],
+		correctChoice: "Wilt Chamberlain",
+	},
+	{
+		question: "Who was the first player in NBA history to be elected league MVP by a unanimous vote?",
+		choices: ['Lebron James', 'Magic Johnson', 'Michael Jordan', 'Stephen Curry'],
+		correctChoice: "Stephen Curry",
+	},	
+	{
+		question: "What new kind of shot did Joe Fulks score a record 63 points with in one game in 1949?",
+		choices: ['Jump Shot', 'Hook Shot', 'Three-point shot', 'Free Throw'],
+		correctChoice: "Jump Shot",
+	},
+	{
+		question: "Who scored the first three-point basket in NBA history?",
+		choices: ['Chris Ford', 'Gene Stump', 'Larry Bird', 'Wes Unseld'],
+		correctChoice: "Chris Ford",
+	},
+	{
+		question: "Who was the youngest player to score 10,000 points in the NBA?",
+		choices: ['Lebron James', 'Michael Jordan', 'Kobe Bryant', 'Wilt Chamberlain'],
+		correctChoice: "Lebron James",
+	},
+	{
+		question: "What team owns the longest winning streak in NBA history?",
+		choices: ['Los Angeles Lakers', 'Golden State Warriors', 'Miami Heat', 'Chicago Bulls'],
+		correctChoice: "Los Angeles Lakers",
+	},
+	{
+		question: "What player was known as the 'Clown Prince' of the Harlem Globetrotters?",
+		choices: ['Fred "Curly" Neal', 'Goose Tatum', 'Meadowlark Lemon', 'Marques Haynes'],
+		correctChoice: "Meadowlark Lemon",
+	},
+	{
+		question: "Who is the all-time leading scorer in men's college basketball?",
+		choices: ['Larry Bird', 'Pete Maravich', 'Stephen Curry', 'Freeman Williams'],
+		correctChoice: "Pete Maravich",
+	},
+	{
+		question: "Who was the first WNBA player to dunk in a playoff game?",
+		choices: ['Lisa Leslie', 'Michelle Snow', 'Tamika Catchings', "Brittney Griner"],
+        correctChoice: "Brittney Griner",
+    }
+];
 
 
 //click done
